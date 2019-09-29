156063ed5e6bd411a5202f534ffecaaa96175be7
Ramon <mendozaramone@gmail.com>
2019-09-29 17:00:34 -0500

    Add new text to the weather result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The min is ' + body.daily.data[0].temperatureMin + '°C and the max is ' + body.daily.data[0].temperatureMax + '°C.')
         }
     })
 }
